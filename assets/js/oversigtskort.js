88ebc4e34ebe186bf13c8e4c96942b20d6e21d39
JacobBFynbo <151468827+JacobBFynbo@users.noreply.github.com>
2024-06-23 16:29:03 +0200

    Opdateret

--- assets/js/oversigtskort.js
+++ assets/js/oversigtskort.js
@@ -2,44 +2,54 @@ document.addEventListener("DOMContentLoaded", function () {
   const allePrikker = document.querySelectorAll(".prik");
   const popUpBoks = document.getElementById("popUpBoks");
 
-  function visPopUpBoks(prik) {
+  function showPopup(prik) {
     const info = prik.getAttribute("dataInfo");
     const dyreNavn = prik.querySelector("span").innerText;
     const imgUrl = prik.getAttribute("data-img-url");
-    popUpBoks.innerHTML = `
-            <h2>${info}</h2>
-            <img src="${imgUrl}" alt="${dyreNavn}" style="width: 100%; height: auto;">
-        `;
+    const linkUrl = prik.getAttribute("data-link-url");
+    popUpBoks.innerHTML = createPopupContent(info, imgUrl, linkUrl, dyreNavn);
     const rect = prik.getBoundingClientRect();
-    popUpBoks.style.left = rect.left + window.scrollX + 20 + "px";
-    popUpBoks.style.top = rect.top + window.scrollY - 10 + "px";
+    popUpBoks.style.left = `${rect.right + window.scrollX + 10}px`;
+    popUpBoks.style.top = `${rect.top + window.scrollY}px`;
     popUpBoks.style.display = "block";
   }
 
-  allePrikker.forEach(function (prik) {
+  function createPopupContent(info, imgUrl, linkUrl, dyreNavn) {
+    return `
+      <h2>${info}</h2>
+      <img src="${imgUrl}" alt="${dyreNavn}" style="width: 70%; height: auto;">
+      <p>Læs mere om <a href="${linkUrl}" target="_blank">${info}</a></p>
+    `;
+  }
+
+  function addPrikEventListeners(prik) {
     prik.addEventListener("click", function () {
-      visPopUpBoks(prik);
+      showPopup(prik);
     });
     prik.querySelector("span").addEventListener("click", function (e) {
       e.stopPropagation();
-      visPopUpBoks(prik);
+      showPopup(prik);
     });
-  });
+  }
 
-  const alleBokse = document.querySelectorAll(".oversigtskortCards div");
-  alleBokse.forEach(function (boks) {
+  function addBoksEventListeners(boks) {
     boks.addEventListener("click", function () {
       const target = boks.getAttribute("dataTarget");
       const prik = document.querySelector("." + target);
       if (prik) {
-        visPopUpBoks(prik);
+        showPopup(prik);
         window.scrollTo(
           0,
           prik.getBoundingClientRect().top + window.scrollY - 300
         );
       }
     });
-  });
+  }
+
+  allePrikker.forEach(addPrikEventListeners);
+
+  const alleBokse = document.querySelectorAll(".oversigtskortCards div");
+  alleBokse.forEach(addBoksEventListeners);
 
   document.addEventListener("click", function (event) {
     if (
